0a2ba210f4d1f7a401cad01042a872d98f40b907
Camilly Pinheiro <camilly2victoria@gmail.com>
2026-03-16 10:41:09 +0000

    Atualização de metricas

--- models/example/chamadas_gold.sql
+++ models/example/chamadas_gold.sql
@@ -3,11 +3,12 @@ with chamadas as (
 )
 
 select 
-id_unico
---,convert(date,data,103) as Data 
-,TMA as TEMPO_FALANDO
-,`tme ` as TEMPO_FILA
-,STATUS AS INDENTIFICADOR
-,Status_final
-,Atendida_NS
+data
+,sum(tempo_falando)tempo_falando
+,sum(tempo_fila)tempo_fila
+,sum(case when indentificador = 1 then 1 else 0 end)Atendidas
+,sum(case when indentificador = 2 then 1 else 0 end)Abandonadas
+,sum(case when indentificador in (1,2) then 1 else 0 end)Recebidas
+,sum(Atendida_NS)Atendida_NS
 from chamadas
+group by data
--- models/example/chamadas_prata.sql
+++ models/example/chamadas_prata.sql
@@ -3,10 +3,14 @@ WITH chamadas as(
 )
 
 SELECT 
-  *,
-  CASE 
+  id_unico
+,cast(data as date) as Data 
+,TMA as TEMPO_FALANDO
+,`tme ` as TEMPO_FILA
+,STATUS AS INDENTIFICADOR
+,CASE 
     WHEN STATUS = 1 THEN 'Atendidas'
     ELSE 'Abandonadas'
   END AS Status_final
-  ,case when `tme `<= 30 then 1 else 0 end Atendida_NS
+,case when `tme `<= 30 then 1 else 0 end Atendida_NS
 FROM `schema.chamadas`
